35379434549f6fb354ffea8b4e49aca0a243e63e
patrykkrol98 <patryk.krol.98@gmail.com>
2021-12-15 12:45:05 +0100

    create new firebase database

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -13,7 +13,7 @@ export class DataStorageService {
     const recipes = this.recipeService.getRecipes();
     this.http
       .put(
-        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json',
+        'https://recipe-book-98f32-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
         recipes
       )
       .subscribe(response => {
@@ -24,7 +24,7 @@ export class DataStorageService {
   fetchRecipes() {
     return this.http
       .get<Recipe[]>(
-        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
+        'https://recipe-book-98f32-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
       )
       .pipe(
         map(recipes => {
