a2212f9f181ef7be489461670b52f69d0be5f67a
patrykkrol98 <patryk.krol.98@gmail.com>
2022-09-28 20:11:44 +0200

    splitting into many modules

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,7 +11,7 @@ export class RecipeService {
 
   private recipes: Recipe[] = [];
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService) {}
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
