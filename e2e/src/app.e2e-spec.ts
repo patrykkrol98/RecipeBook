1218efc714cfb10a9057ad0c1cb2b46a3835460c
patrykkrol98 <patryk.krol.98@gmail.com>
2022-09-28 21:40:59 +0200

    path fix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-complete-guide-update!');
+    expect(page.getTitleText()).toEqual('Welcome to recipe-book!');
   });
 
   afterEach(async () => {
